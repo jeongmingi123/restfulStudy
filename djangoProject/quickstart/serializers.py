57b6c61f4b0357dead9f847ead8f5777aeb5ff67
jeongmingi <mo4811@naver.com>
2021-03-06 16:37:59 +0900

    수정

---
+++ djangoProject/quickstart/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User, Group
+from .models import Post
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title','text','date','host']
+
---
+++ djangoProject/quickstart/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User, Group
+from .serializers import UserSerializer, GroupSerializer, PostSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Post
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
