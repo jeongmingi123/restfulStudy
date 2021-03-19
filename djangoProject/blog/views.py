a6a96a3779af4512247bc77a378eea915585b6e3
jeongmingi <mo4811@naver.com>
2021-03-19 22:29:08 +0900

    from django.views import View 사용

--- djangoProject/blog/views.py
+++ djangoProject/blog/views.py
@@ -1,50 +1,111 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from . import models
+from django.views import View
+
+# View 만 이용해서 사용
+class All_blog(View):
+
+    def get(self, request):
+        all_blogs = models.Blog.objects.all()
+        return render(request, "all_views.html", context={"blogs": all_blogs})
 
 
-def all_blog(request):  # 모든 블로그 내용
+class Bloging(View):
 
-    all_blogs = models.Blog.objects.all()
-
-    # context 첫번째 인자는 html 템플릿에 보낼 변수이름, 두번째는 함수에서 보낼 변수
-    return render(request, "all_views.html", context={"blogs": all_blogs})
-
-
-def bloging(request, pk):
-    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
-    blog = models.Blog.objects.get(pk=pk)
-
-    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
-    return render(request, 'bloging.html', context={'blog': blog})
+    def get(self, request, pk):
+        blog = models.Blog.objects.get(pk=pk)
+        return render(request, 'bloging.html', context={'blog': blog})
 
 
-def new_blog(request):  # 요청된 방법이 POST 방식이면, create 메소드 사용하여, DB 생성
+class New_blog(View):
 
-    if request.method == 'POST':
+    def get(self, request):
+        return render(request, 'new_blog.html')
+
+    def post(self, request):
         new_blog = models.Blog.objects.create(
             title=request.POST['title'],
             text=request.POST['text'],
             comment=request.POST['comment'],
         )
         return redirect('/blog/')
-    return render(request, 'new_blog.html')
 
 
-def remove_blog(request, pk):
-    blog = models.Blog.objects.get(pk=pk)
+class Remove_blog(View):
 
-    if request.method == 'POST':
+    def get(self, request, pk):
+        blog = models.Blog.objects.get(pk=pk)
         blog.delete()
         return redirect('/blog/')
-    return render(request, 'remove_blog.html', {'blog': blog})
 
 
-def edit_blog(requset, pk):
-    blog = models.Blog.objects.get(pk=pk)
-    if requset.method == 'POST':
-        blog.title = requset.POST['title']
-        blog.text = requset.POST['text']
-        blog.comment = requset.POST['comment']
+class Edit_blog(View):
+
+    def get(self, request, pk):
+        blog = models.Blog.objects.get(pk=pk)
+        return render(request, 'edit_blog.html', {'blog': blog})
+
+    def post(self, request, pk):
+        blog = models.Blog.objects.get(pk=pk)
+        blog.title = request.POST['title']
+        blog.text = request.POST['text']
+        blog.comment = request.POST['comment']
         blog.save()
         return redirect(f'/blog/{blog.pk}')
-    return render(requset, 'edit_blog.html', {'blog': blog})
+
+
+
+# 이전 FBV 내용
+
+# # 모든 블로그 글 내용 메소드
+# def all_blog(request):
+#
+#     all_blogs = models.Blog.objects.all()
+#
+#     # context 첫번째 인자는 html 템플릿에 보낼 변수이름, 두번째는 함수에서 보낼 변수
+#     return render(request, "all_views.html", context={"blogs": all_blogs})
+#
+#
+# # 블로그 개인 작성이 작성한 내용을 보여주는 메소드
+# def bloging(request, pk):
+#     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+#     blog = models.Blog.objects.get(pk=pk)
+#
+#     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+#     return render(request, 'bloging.html', context={'blog': blog})
+#
+#
+# # 새로운 블로그 글 생성하는 메소드
+# def new_blog(request):
+#     if request.method == 'POST': # 요청된 방법이 POST 방식이면, create 메소드 사용하여, DB 생성
+#         new_blog = models.Blog.objects.create(
+#             title = request.POST['title'],
+#             text = request.POST['text'],
+#             comment = request.POST['comment'],
+#         )
+#         return redirect('/blog/')
+#     return render(request, 'new_blog.html')
+#
+#
+# # 블로그 글 지우는 메소드
+# def remove_blog(request, pk):
+#
+#     blog = models.Blog.objects.get(pk=pk)
+#
+#     if request.method == 'POST':
+#         blog.delete()
+#         return redirect('/blog/')
+#     return render(request, 'remove_blog.html', {'blog': blog})
+#
+#
+# # 블로그 글 수정하는 메소드
+# def edit_blog(requset, pk):
+#     blog = models.Blog.objects.get(pk=pk)
+#     if requset.method == 'POST':
+#         blog.title = requset.POST['title']
+#         blog.text = requset.POST['text']
+#         blog.comment = requset.POST['comment']
+#         blog.save()
+#         return redirect(f'/blog/{blog.pk}')
+#     return render(request, 'edit_blog.html', {'blog': blog})
--- djangoProject/restfulApi/urls.py
+++ djangoProject/restfulApi/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
 from quickstart import views
-from blog.views import all_blog as blog_views
-from blog.views import bloging, new_blog, remove_blog, edit_blog
+from blog.views import All_blog as blog_views
+from blog.views import Bloging, New_blog, Remove_blog, Edit_blog
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -17,10 +17,10 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    path('blog/', blog_views, name='blog'),
-    path('blog/<int:pk>', bloging, name="bloging"),
-    path('blog/new_blog', new_blog, name="new_blog"),
-    path('blog/<int:pk>/remove/', remove_blog),
-    path('blog/<int:pk>/edit/', edit_blog)
+    path('blog/', blog_views.as_view(), name='blog'),
+    path('blog/<int:pk>', Bloging.as_view(), name="bloging"),
+    path('blog/new_blog', New_blog.as_view(), name="new_blog"),
+    path('blog/<int:pk>/remove/', Remove_blog.as_view()),
+    path('blog/<int:pk>/edit/', Edit_blog.as_view())
 
 ]
